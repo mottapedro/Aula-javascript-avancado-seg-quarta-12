46ad6b7e243d6b8145c46e661d8e269006efdc0c
mottapedro <mottapedro01@gmail.com>
2023-03-13 23:46:49 -0300

    Commit : getById, updateOneTask

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,33 +1,56 @@
-const Task = require('../models/Task');
+const Task = require('../models/Task')
 
-
-const getALLTasks = async (req, res) => {//requisição para trazer as tarefas e renderizar na pagina html
-
+const getAllTasks = async (req, res) => {//modulo criado 
     try {
         const taskList = await Task.find();
-        res.render("index", { taskList });
+        return res.render("index", { taskList, task: null });
     } catch (err) {
         res.status(500).send({ error: err.message });
     }
-
 };
 
-const createTask = async (req, res) => {//reuisição para gravar as tarefas no banco
+const createTask = async (req, res) => {
     const task = req.body;
 
     if (!task.task) {
         return res.redirect("/");
     }
 
+
+
     try {
-        await Task.create(task);
-        return res.redirect("/");
+        await Task.create(task)
+        return res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const getById = async (req, res) => {
+    //enviar o id como parametro da rota
+    try {
+        const task = await Task.findOne({ _id: req.params.id });
+        const taskList = await Task.find();
+        res.render("index", { task, taskList });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const updateOneTask = async (res, req) => {
+    try {
+
+        const task = req.body;
+        await Task.updateOne({ _id: req.params.id }, task);
+        res.redirect("/");
     } catch (err) {
         res.status(500).send({ error: err.message });
     }
 }
 
 module.exports = {
-    getALLTasks,
-    createTask
-}
+    getAllTasks,
+    createTask,
+    getById,
+    updateOneTask
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
-const routes = require('express').Router();
-const TaskController = require("../controller/TaskController")
+const routes = require("express").Router();
+const TaskController = require("../controller/TaskController");
 
 
-routes.get("/", TaskController.getALLTasks);
+routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
-
+routes.get("/getById/:id", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
 
 module.exports = routes;
